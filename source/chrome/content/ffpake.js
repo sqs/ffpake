6dde8d15b5563cd7e41d2ff602f8663694215562
sqs <qslack@qslack.com>
2010-07-21 14:59:51 -0700

    receive notification and register PAKEAuthProfile profile

--- source/chrome/content/ffpake.js
+++ source/chrome/content/ffpake.js
@@ -2,31 +2,43 @@
 
 Cu.import("resource://weave-identity/ext/log4moz.js");
 Cu.import("resource://weave-identity/ext/Observers.js");
+Cu.import("resource://weave-identity/profilemanager.js");
+Cu.import("resource://ffpake/profiles/http-pake-auth.js");
+
 
 function FFPake() {
     this._log = Log4Moz.repository.getLogger("FFPake");
-    this._log.level = Log4Moz.Level[Svc.Prefs.get("log.logger.profiles")];
+    this._log.level = "All";
 }
 
-FFPake.startup = function() {
-    Observers.add("weaveid-profile-manager-start", this, false);
-    alert("hello");
-};
-
-FFPake.shutdown = function() {
-    Observers.remove("weaveid-profile-manager-start", this, false);
-};
-
-FFPake.prototype = {    
-    observe: function(subject, topic, data) {
-        this._log.debug("observe: topic: " + topic);
-        if (topic != "weaveid-profile-manager-start") {
-            return;
-        }
+FFPake.prototype = {
+    startup: function() {
+        Observers.add("weaveid-profile-manager-start", this, false);
+        this._log.debug("Observing weaveid-profile-manager-start");
     },
-}
+
+    shutdown: function() {
+        Observers.remove("weaveid-profile-manager-start", this, false);
+    },
+
+    observe: function(subject, topic, data) {
+        this._log.debug("observe: topic: " + topic + 
+                        " subject: " + subject);
+        if (topic == "weaveid-profile-manager-start") {
+            /* Add HTTP PAKE Auth profile. */
+            this._log.debug("Going to register PAKEAuthProfile");
+            subject.registerProfile(PAKEAuthProfile);
+            this._log.debug("Registered PAKEAuthProfile");
+        }
+    }
+};
+
 
 // Install load and unload handlers
-window.addEventListener("load", function(e) { FFPake.startup(); }, false);
-window.addEventListener("unload", function(e) { FFPake.shutdown(); }, false);
+var ffpake;
+window.addEventListener("load", function(e) { 
+        ffpake = new FFPake(); 
+        ffpake.startup(); }, 
+    false);
+window.addEventListener("unload", function(e) { ffpake.shutdown(); }, false);
 
